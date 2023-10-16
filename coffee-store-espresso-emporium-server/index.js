ab56e90dc9a3eb5794bf12db3f39cde84bf8664e
Mahfujur-Rahman6793 <mahfujurrahman6793@gmail.com>
2023-10-17 01:48:14 +0600

    initial setup for server site

---
+++ coffee-store-espresso-emporium-server/index.js
@@ -0,0 +1,53 @@
+// Initialization and require. Must be require which are you install
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewareWrapper
+
+app.use(cors());
+app.use(express.json());
+
+// Dynamic uri for mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pngg0qg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// Basic route
+
+app.get('/',(req,res)=>{
+    res.send('after some times maybe coffe is coming');
+})
+
+// attachment into the port
+
+app.listen(port,()=>{
+    console.log(`apllication is running on ${port}`);
+})
